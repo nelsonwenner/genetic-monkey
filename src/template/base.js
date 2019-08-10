84db2e83b92bd39621506e678ee11c915adfd86a
unknown <nelsondiasdemedeiros@gmail.com>
2019-08-10 15:21:51 -0300

    Feractor: Aplicando estilização, com flex-box deixando responsivo para mobile.

--- src/template/base.js
+++ src/template/base.js
@@ -1,13 +1,11 @@
 
 nav = () => {
     let dados = `
-        <div class="navbar-dark fixed-top bg-dark">
-            <nav class="navbar container navbar-expand-lg">
-                <img class="circle" id="img" height="60" width="60" src="./src/assets/img/monkey-laptop.png"/>
-                <div class="collapse navbar-collapse">
-                </div>
-            </nav>
+    <header class="bg-color">
+        <div class="my-container">
+            <img class="circle logo" id="img" height="60" width="60" src="./src/assets/img/monkey-laptop.png"/>
         </div>
+    </header>
     `
     const nav = document.getElementById("nav");
     nav.innerHTML = dados;
--- src/template/body.js
+++ src/template/body.js
@@ -1,70 +1,57 @@
 
 body = (lengthPopulation, phrase) => {
     let data = `
-    <main class="mt-5">
-        <div class="col mt-5"></div>
-        <div class="col mt-5"></div>
-        <div class="col mt-5"></div>
-        <div class="card container mt-5">
-            <div class="row ">
-                <div class="col col-lg-4 col-sm-4 col-md-12 mt-2 mb-2">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="text-center">
-                                <img class="rounded-circle" id="img" height="110" width="110" src="./src/assets/img/monkeyChild.gif"/>
-                            </div>
-                            <div class="card mt-3 pl-2">
-                                <a class="pt-2" id="useNewFont" >Population: ${ lengthPopulation } </a>
-                                <a class="pb-2" id="useNewFont" >Phrase: ${ phrase } </a>
-                            </div>
-                            <div class="mt-2">
-                                <input class="form-control" type="text" id="phrase" placeholder="Enter the phrase">
-                                <input class="form-control mt-1" type="number" id="lengthPoputation" placeholder="Population quantity">
-                                <button class="btn btn-dark btn-sm btn-block my-2" id="add" type="button">Add</button>
-                            </div> 
-                            <div class="mt-2">
-                                <button class="btn btn-success btn-sm btn-block my-2" id="run" type="button">Execute</button>
-                            </div>
-                            <div class="mt-2">
-                                <button class="btn btn-info btn-sm btn-block my-2" id="reload" type="button">Reload</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-lg-4 col-sm-8 col-md-12 mt-2">
-                    <div class="card">
-                        <div class="card-header" style="height: 430px">
-                            <div class="text-center" id="soupWord">
-                                <h3 class="pt-2 text-center" id="useNewFont">
-                                    Word soup
-                                </h3>
-                                    <br><img text-center" height="300" width="280" src="./src/assets/img/monkeyCrazy.gif"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-lg-4 col-sm-12 col-md-12 mt-2">
-                    <div class="card">
-                        <div class="card-header" style="height: 430px">
-                            <div id="info">
-                                <h3 class="pt-2 text-center" id="useNewFont">
-                                    Generation <br>0
-                                </h3>
-                                <h3 class="pt-2 text-center" id="useNewFont">
-                                    Fitness <br>0
-                                </h3>
-                                <h3 class="pt-2 text-center" id="useNewFont">
-                                    Phrase <br>Empty
-                                </h3>
-                                <div class="text-center pt-5 pl-5" id="monkeyCool">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
+    <main class="margin-top-110">
+		<div class="my-container-flex ">
+			<div class="card-style">
+				<div class="container-flex-com-wrap">
+					<div class="card-item-flex">
+						<div class="monkey">
+							<img class="rounded-circle perfil-monkey" id="img" height="110" width="110" src="./src/assets/img/monkeyChild.gif"/>
+						</div>
+						<div class="margin-button">
+							<div class="card-style margin-10 fontSizeLow">
+								<a class="" id="useNewFont" >Population: ${ lengthPopulation } </a><br>
+								<a class="" id="useNewFont" >Phrase: ${ phrase } </a>
+							</div>
+						</div>
+						<div class="margin-input">
+							<input class="input" type="text" id="phrase" placeholder="Enter the phrase">
+						</div>
+						<div class="margin-input">
+							<input class="input" type="number" id="lengthPoputation" placeholder="Population quantity">
+							<button class="buttonn btn-dark margin-button-10" id="add" type="button">Add</button>
+							<button class="buttonn btn-success" id="run" type="button">Execute</button>
+							<button class="buttonn btn-info" id="reload" type="button">Reload</button>
+						</div>
+					</div>
+					<div class="card-item-flex">
+						<div class="wordSoup" id="soupWord">
+							<h3 id="useNewFont">
+								Word soup
+							</h3>
+							<br><img height="300" width="280" src="./src/assets/img/monkeyCrazy.gif"/>
+						</div>
+					</div>
+					<div class="card-item-flex">
+						<div class="info" id="info">
+							<h3 id="useNewFont">
+								Generation <br>0
+							</h3>
+							<h3 id="useNewFont">
+								Fitness <br>0
+							</h3>
+							<h3 id="useNewFont">
+								Phrase <br>Empty
+							</h3>
+                            <div class="monkeyCool" id="monkeyCool">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
     `
     const body = document.getElementById("body");
     body.innerHTML = data;
@@ -72,19 +59,19 @@ body = (lengthPopulation, phrase) => {
 
 informationRun = (generation, bestFitness, bestDna) => {
     let data = `
-        <div id="info">
-            <h3 class="pt-2 text-center" id="useNewFont">
-                Generation <br>${ generation }
-            </h3>
-            <h3 class="pt-2 text-center" id="useNewFont">
-                Fitness <br>${ bestFitness }
-            </h3>
-            <h3 class="pt-2 text-center" id="useNewFont">
-                Phrase <br>${ bestDna }
-            </h3>
-            <div class="text-center pt-5 pl-5" id="monkeyCool">
-            </div>
+    <div class="info" id="info">
+        <h3 id="useNewFont">
+            Generation <br>${ generation }
+        </h3>
+        <h3 id="useNewFont">
+            Fitness <br>${ bestFitness }
+        </h3>
+        <h3 id="useNewFont">
+            Phrase <br>${ bestDna }
+        </h3>
+        <div id="monkeyCool">
         </div>
+    </div>
     `
     const info = document.getElementById("info");
     info.innerHTML = data;
@@ -105,7 +92,9 @@ soupWords = (allGenes) =>{
 
 monkeyCool = () => {
     let data = `
+    <div class="monkeyCool" id="monkeyCool">
         <img height="130" width="150" src="./src/assets/img/monkeyCool.gif"/>
+	</div> 
     `
     const monkeyCool = document.getElementById("monkeyCool");
     monkeyCool.innerHTML = data;
